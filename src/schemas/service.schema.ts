fc7ce6e6023a124758d8024cd3b22b53193c69f2
michaelyev <michaelyevt@gmail.com>
2024-06-10 21:27:41 +0300

    asasasa

--- src/schemas/service.schema.ts
+++ src/schemas/service.schema.ts
@@ -1,6 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+interface WarrantySection {
+  image: string;
+  imageAlt: string;
+  phrase: string;
+  h2: string;
+  paragraph1: string;
+  paragraph2: string;
+  price: string;
+}
+
 @Schema()
 export class ServiceSub extends Document {
   @Prop({ required: true })
@@ -20,75 +30,72 @@ export class ServiceSub extends Document {
 
   @Prop({
     required: true,
-    type: {
-      h1: String,
-      heroH2: String,
-      heroP: String,
-      heroImageAlt: String,
-    },
+    type: Object,
   })
   hero: {
     h1: string;
-    heroH2: string;
+    heroPhrase: string;
     heroP: string;
     heroImageAlt: string;
   };
 
   @Prop({
     required: true,
-    type: {
-      image1: String,
-      image1alt: String,
-      image2: String,
-      priceLine1: String,
-      priceLine2: String,
-      image2alt: String,
-      sectionH2: String,
-      sectionP1: String,
-      sectionP2: String,
-      section2H2: String,
-      section2P1: String,
-      section2P2: String,
-      perks: [
-        {
-          heading: String,
-          paragraph: String,
-        },
-      ],
-    },
+    type: Object,
   })
   about: {
-    image1: string;
-    image1alt: string;
+    phrase1: string;
+    phrase2: string;
     priceLine1: string;
     priceLine2: string;
+    image1: string;
+    image1alt: string;
     image2: string;
     image2alt: string;
-    sectionH2: string;
+    sectionH1: string;
     sectionP1: string;
     sectionP2: string;
     section2H2: string;
     section2P1: string;
     section2P2: string;
-    perks: { heading: string; paragraph: string }[];
+    perks: Array<{
+      heading: string;
+      paragraph: string;
+    }>;
   };
 
   @Prop({
     required: true,
-    type: [
-      {
-        id: Number,
-        title: String,
-        description: {
-          p1: String,
-          p2: String,
-          p3: String,
-        },
-        imagePath: String,
-      },
-    ],
+    type: Array,
   })
-  howWeWork: {
+  ourServices: Array<{
+    h2: string;
+    paragraph: string;
+    array: Array<{
+      heading: string;
+      paragraph: string;
+      url: string;
+      image: string;
+      price: string;
+    }>;
+    servicesMarkdown: string;
+  }>;
+
+  @Prop({
+    required: true,
+    type: Object,
+  })
+  manufacturers: {
+    h2: string;
+    p: string;
+    markdownmanufacturers: string;
+  };
+
+  @Prop({
+    required: true,
+    type: Array,
+  })
+  howWeWork: Array<{
     id: number;
     title: string;
     description: {
@@ -97,45 +104,21 @@ export class ServiceSub extends Document {
       p3: string;
     };
     imagePath: string;
-  }[];
+  }>;
 
   @Prop({
     required: true,
-    type: [
-      {
-        heading: String,
-        paragraph: String,
-        url: String,
-        image: String,
-        price: String,
-      },
-    ],
+    type: Array,
   })
-  servicesSlider: {
-    heading: string;
-    paragraph: string;
-    url: string;
-    image: string;
-    price: string;
-  }[];
-
-  @Prop({
-    required: true,
-    type: [
-      {
-        question: String,
-        answer: String,
-      },
-    ],
-  })
-  faqItems: {
+  faqItems: Array<{
     question: string;
     answer: string;
-  }[];
+  }>;
 
-  @Prop({ required: true })
-  markdown: string;
+  @Prop({ required: true, type: Map })
+  warrantySection: WarrantySection;
 }
+
 
 
 export const ServiceSubSchema = SchemaFactory.createForClass(ServiceSub);
